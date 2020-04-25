0ec780a1d5e44be3ba2f4b5f33c6bc157798a4d4
Ustimenko Denis <UstimenkoDenis@mail.ru>
2020-04-25 19:01:44 +0300

    Counter React Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import App from './components/app';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import counter from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//  создаем хранилище
+const store = createStore(counter);
+
+ReactDom.render(<Provider store = {store}>
+                     <App/>
+                </Provider>, document.getElementById('root'));
